e9ec644af5df036eb2085a47d016586a71173aec
Anubhav Jaiswal <anubhavjaiswal2002@gmail.com>
2026-05-01 00:52:20 +0530

    feat: remove unused components and clean up Crew and Dashboard pages

--- frontend/src/components/common/TrackDivider.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const TrackDivider = ({ className = '' }) => {
-  return (
-    <div className={`track-divider w-full ${className}`}></div>
-  );
-};
-
-export default TrackDivider;
